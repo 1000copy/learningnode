04a7ffa6335992f236ae99cf9e568cf433e2800e
liu chuan jun <1000copy@gmail.com>
2015-06-15 16:59:31 +0800

    -

--- douban.readed/app.js
+++ douban.readed/app.js
@@ -13,11 +13,7 @@ function hackhtml(text){
         var $element = $(element);
         var c = childBy2(element,'div','title')
         var d = childBy1(c,'a')
-        // console.log(d.children[0].data.trim())            
-        // console.log(d.attribs.href.trim())                 
         var e = childBy2(element,'div','date')
-        // console.log(d)
-        // console.log(joinData(e).trim())
         items.push({
           title:d.children[0].data.trim(),
           href:d.attribs.href.trim(),
@@ -32,13 +28,11 @@ function joinData(element){
   var r  =''
   for(var i=0;i<element.children.length;i++){
     var c =  element.children[i]
-    if (c.type =='text' ){
-      // console.log(c)
+    if (c.type =='text' ){      
         r += c.data
     }    
   }
-  return r;
-  // return undefined
+  return r;  
 }
 
 
@@ -59,12 +53,14 @@ function childBy1(element,tag){
   return undefined
 }
 url = 'https://cnodejs.org/'
-url = "http://book.douban.com/people/1830596/collect?sort=time&start=159&filter=all&mode=list&tags_sort=count"
+url = "http://book.douban.com/people/1830596/collect?sort=time&filter=all&mode=list&tags_sort=count&count=100&start=0"
 superagent.get(url)
   .end(function (err, sres) {
     if (err) {
       return next(err);
     }
-    console.log (hackhtml(sres.text))    
+    var r = hackhtml(sres.text)        
+    console.log(r)
   });
+
 
