ead272ca5c990c0ccb391a5955a304522d637fed
1000copy <1000copy@gmail.com>
2015-06-16 22:13:40 +0800

    add useragent

--- douban.readed/a3.js
+++ douban.readed/a3.js
@@ -67,6 +67,7 @@ function fetch(start){
   url += start
 
   superagent.get(url)
+    .set("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36")
     .end(function (err, sres) {
       console.log("*")
       if (err) {
