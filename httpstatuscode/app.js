0b33a2be27e08118214254402195b1e0c8351dbf
1000copy <1000copy@gmail.com>
2015-12-03 17:14:52 +0800

    archieve for google keep

--- httpstatuscode/app.js
+++ httpstatuscode/app.js
@@ -2,16 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<a href="/test204">204</a> <a href="/test205">205</a>');
+  res.send('<a href="/test204">204</a> <a href="/test205">205</a> <a href="/test300">300</a>');
 });
 
 app.get('/test204', function (req, res) {
-  res.status(204).send("Hello World!")
+  res.status(204)
+  res.set("Location","/a")
+  res.send("Hello World!")
 });
-
+ 
 app.get('/test205', function (req, res) {
   res.status(205)
 });
+
+app.get('/test300', function (req, res) {
+  res.status(300).send("<a href='/a1'>a1</a> <a href='/a2'>a2</a>")
+});
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
