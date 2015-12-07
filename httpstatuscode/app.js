016ecf019fc68c03b6979a27b0d96139d8926692
1000copy <1000copy@gmail.com>
2015-12-07 12:54:05 +0800

    get ,head,post ,test suite

--- httpstatuscode/app.js
+++ httpstatuscode/app.js
@@ -21,8 +21,5 @@ app.get('/test300', function (req, res) {
 
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('listening on 3000',);
 });
