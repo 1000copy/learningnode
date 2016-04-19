1d379218be96fca5ee3bdf7119e48d9d03ea2d4c
1000copy <1000copy@gmail.com>
2016-04-19 16:27:41 +0800

    add pandoc script

---
+++ nodebook/gitbook/gitbook.sh
@@ -0,0 +1,15 @@
+pandoc -o gitbook.epub  1.\ readme.md \
+                      2.\ ready.md \
+                      basic/1.\ intro.md \
+                      basic/2.\ stage.md \
+                      basic/3.\ undo.md \
+                      basic/4.\ branch.md  \
+                      basic/5.\ tag.md \
+                      basic/6.\ hist.md \
+                      basic/7.\ model.md \
+                      basic/8.\ rebase.md\
+                      basic/9.\ cherry-pick.md\
+                      more/1.\ duo\ repo.md \
+                      more/2.\ protocols.md  \
+                      9.\ paperback.md \
+ --toc --toc-depth=2 
