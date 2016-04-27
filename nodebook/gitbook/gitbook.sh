eb25264b5032dfcbe4eeaadd38a06380ce88bec1
1000copy <1000copy@gmail.com>
2016-04-27 19:17:18 +0800

    hist scan

--- nodebook/gitbook/gitbook.sh
+++ nodebook/gitbook/gitbook.sh
@@ -4,12 +4,12 @@ pandoc -o gitbook.epub  1.readme.md \
                       basic/2.stage.md \
                       basic/3.undo.md \
                       basic/4.branch.md  \
-                      basic/5.tag.md \
                       basic/6.hist.md \
                       basic/7.model.md \
                       basic/8.rebase.md\
                       basic/9.cherry-pick.md\
                       more/1.duorepo.md \
                       more/2.protocols.md  \
+                      more/5.tag.md \
                       9.paperback.md \
  --toc --toc-depth=2 
