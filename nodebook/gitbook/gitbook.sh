4c089bf49ab24d48100003a073a3662737ecfb8e
1000copy <1000copy@gmail.com>
2016-05-12 12:15:52 +0800

    rebase SCAN 1

--- nodebook/gitbook/gitbook.sh
+++ nodebook/gitbook/gitbook.sh
@@ -5,11 +5,15 @@ pandoc -o gitbook.epub  1.readme.md \
                       basic/3.undo.md \
                       basic/4.branch.md  \
                       basic/6.hist.md \
-                      basic/7.model.md \
-                      basic/8.rebase.md\
-                      basic/9.cherry-pick.md\
-                      more/1.duorepo.md \
-                      more/2.protocols.md  \
+                      basic/7.revision.md \
+                      basic/8.duorepo.md \
+                      more/readme.md \
+                      more/8.rebase.md\
+                      more/9.cherry-pick.md\
                       more/5.tag.md \
+                      protocols/readme.md \
+                      protocols/1.git.md \
+                      protocols/2.ssh.md \
+                      protocols/3.http.md \
                       9.paperback.md \
  --toc --toc-depth=2 
