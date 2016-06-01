453fc800a02ff37e81d46d2e45107c16c7a5d50f
1000copy <1000copy@gmail.com>
2016-06-01 10:40:21 +0800

    ch1 section1-2

--- nodebook/gitbook/gitbook.sh
+++ nodebook/gitbook/gitbook.sh
@@ -4,7 +4,6 @@ pandoc -o gitbook.epub  1.readme.md \
                       basic/2.stage.md \
                       basic/3.undo.md \
                       basic/4.branch.md  \
-                      basic/6.hist.md \
                       basic/7.revision.md \
                       basic/8.duorepo.md \
                       more/5.tag.md \
